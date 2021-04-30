5fb7c3edf075c3e0374e106e845b6f85086fd5d9
Rytis <rytiskacinskis@gmail.com>
2021-04-30 12:07:19 +0300

    Add project files.

---
+++ WorkFree/Areas/Identity/Data/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace WorkFree.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the User class
+    public class User : IdentityUser
+    {
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+
+        [Column(TypeName = "nvarchar(500)")]
+        public string ProfileDescription { get; set; }
+    }
+}
---
+++ WorkFree/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WorkFree.Areas.Identity.Data;
+using WorkFree.Data;
+
+[assembly: HostingStartup(typeof(WorkFree.Areas.Identity.IdentityHostingStartup))]
+namespace WorkFree.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<WorkFreeContext>(options =>
+                    options.UseMySQL(
+                        context.Configuration.GetConnectionString("WorkFreeContextConnection")));
+
+                services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<WorkFreeContext>();
+            });
+        }
+    }
+}
